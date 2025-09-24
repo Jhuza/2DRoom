b279803fb0d2d4a8a96afa4a63768bc1b7dd225a
Jhuza <contactojhg18@gmail.com>
2025-09-23 19:36:42 -0500

    Notifications

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using System;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using TMPro; //libreria para usar textos
 
 public class PlayerController : MonoBehaviour
 {
@@ -8,12 +9,17 @@ public class PlayerController : MonoBehaviour
     public int score = 0; //variable para guardar la puntuación
     public bool hasKey = false; //variable para guardar si tiene la llave
     public bool hasWater = false; //variable para guardar si tiene el agua
+    public TextMeshProUGUI scoreText; //variable para el texto de la puntuación
+    public TextMeshProUGUI notificationText; //variable para el texto de notificaciones
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        UpdateTextScore();
 
+
+        notificationText.text = "Collect 7 items to win!";
     }
 
     // Update is called once per frame
@@ -36,18 +42,21 @@ public class PlayerController : MonoBehaviour
         if(other.CompareTag("Collectable"))
         {
             score = score + 1; //aumentar la puntuación
-            
+            UpdateTextScore();
 
             Destroy(other.gameObject); //destruir el objeto con el que colisionamos
             Debug.Log("Objeto recogido");
             Debug.Log("Score:  "+ score);
             if(score >= 7)
                 Debug.Log("Recoje la llave!");
+            notificationText.text = "Score: " + score + "/7";
 
         } 
         else
         {
             Debug.Log("Sigue intentando, necesitas 7 para ganar");
+
+            notificationText.text = "Score: " + score + "/7";
 
         }
         if ( other.CompareTag("Key"))
@@ -56,12 +65,16 @@ public class PlayerController : MonoBehaviour
             Debug.Log("Has recogido la llave, ya puedes abrir la puerta");
             Destroy(other.gameObject); //destruir el objeto con el que colisionamos
 
+            notificationText.text = "You have the key! Now go to the door!";
+
         }
         if (other.CompareTag("Water"))
         {
             hasWater = true; 
             Debug.Log("Has tocado el agua y perdiste");
             Destroy(gameObject); 
+
+            notificationText.text = "You touched the water and lost!";
 
         }
 
@@ -75,7 +88,13 @@ public class PlayerController : MonoBehaviour
         else
         {
             Debug.Log("No puedes abrir la puerta, te faltan objetos por recoger");
+
+            notificationText.text = "You need 7 items and the key to win!";
         }
 
+    }
+    void UpdateTextScore()
+    {
+        scoreText.text = "Score: " + score;
     }
 }
